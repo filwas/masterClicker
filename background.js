d0d16ab33650e2cbf8cbaa5e8bab2394e8536a08
Filip Was <fwas.corp@gmail.com>
2023-05-19 18:42:23 +0200

    extension logs class name to console

--- background.js
+++ background.js
@@ -1,9 +1,47 @@
 // Listen for messages from the content script
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((message) => {
     if (message.elementClicked) {
       // Perform desired actions with the target element
-      console.log("Element clicked:", message.elementClicked);
+
+        var className = message.elementClicked.match(/class="(.+?)"/)
+                            
+
+        console.log(className[0].replace(/^.{7}/).replace(".$"));
+        console.log(message.elementClicked);
       // You can send this information to your server, store it locally, or perform any other operations.
+
+      let queryOptions = {
+        currentWindow: true,
+        url: "https://cockpit-sta.airhelp.com/*"
+        }   
+
+        /*chrome.tabs.query(queryOptions).then(
+            (fulfill) => {
+                fulfill.forEach(tab => {
+                    let id = tab.id;
+                    chrome.scripting.executeScript({
+                        target: {tabId: id},
+                        func: () => {
+                            let eleID = message.elementClicked.id;
+                            if (eleID) {
+                                document.getElementById(eleID).click();
+                                return;
+                            } else {
+                                let eleClass = message.elementClicked.className;
+                                document.getElementsByClassName(eleClass)[0].click();
+                                return;
+                            }
+                        }
+                    });
+                });
+            }
+
+        )*/
+
+
+
+
+
     }
   });
   
